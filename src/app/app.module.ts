ab4db7449693de1f15a334ca8b7879c010997f2a
williamfernandessilva <willgsvs@hotmail.com>
2024-04-24 16:33:13 -0300

    linkado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent {
+
+}
