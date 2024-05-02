b5c34b7679c8718420521463daa059974a671262
kauesimao <kauesimao1234@gmail.com>
2024-05-02 20:19:23 -0300

    quase finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Importe FormsModule
 
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
-    CalculadoraComponent
+    FormsModule // Adicione FormsModule aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
-  standalone: true,
-  imports: [],
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
-
+  num1: number = 0;
+  num2: number = 0;
+  resposta: number = 0;
+  operacao() {
+    this.resposta = this.num1 / this.num2;
+  }
 }
